28b7e42eee36b2c7b0dbafea860cf7b1d4f7564e
sangjin0214 <35863587+sangjin0214@users.noreply.github.com>
2024-02-29 05:11:36 +0900

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,6 @@ import sys
 from YoutubeDownloader import downloader
 from BlankGenerator import testpage, answerpage
 from application_confirm import check_info
-import os
 
 
 application = Flask(__name__)
@@ -25,20 +24,12 @@ def browse():
 
 @application.route("/")
 def hello():
-    '''
-    jokbo_list = os.listdir('/app/')
-    
-    for i in range(len(jokbo_list)):
-        jokbo_list[i] = [jokbo_list[i], os.listdir('/'+str(jokbo_list[i])+'/')]
-    '''
     html = ("Welcome to Sangjin's lab <br>"
             "<a href='/youtube/'>Youtube Download</a><br>"
             "<a href='/blankgenerator/'>Blank Generator</a><br>"
             "<a href='/application_confirm/'>Application Confirm</a>"
             "<br><br><br>//update log<br>blankgenerator : level of difficulty added"
             "<br>application_confirm : added")
-'''            "<br>"+str(jokbo_list))
-'''
     return html
 
 
